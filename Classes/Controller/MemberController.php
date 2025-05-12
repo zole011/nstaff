4e10c3d766e7521c72ee0d82ed0832d349ed551d
zole011 <zoran.it@gmail.com>
2025-05-12 23:06:09 +0200

    new commit

--- Classes/Controller/MemberController.php
+++ Classes/Controller/MemberController.php
@@ -15,16 +15,35 @@ class MemberController extends ActionController
         $this->memberRepository = $memberRepository;
     }
 
-    public function listAction(): ResponseInterface
-    {
-        $members = $this->memberRepository->findAll();
-        $this->view->assign('members', $members);
-        return $this->htmlResponse();
-    }
+    //public function listAction(): ResponseInterface
+    //{
+     //   $members = $this->memberRepository->findAll();
+    //    $this->view->assign('members', $members);
+    //    return $this->htmlResponse();
+    //}
+    public function listAction(): \Psr\Http\Message\ResponseInterface
+{
+    $members = $this->memberRepository->findAll();
+    $this->view->assign('members', $members);
+    return $this->htmlResponse();
+}
 
     public function showAction(Member $member): ResponseInterface
     {
         $this->view->assign('member', $member);
         return $this->htmlResponse();
     }
+    public function newAction(): \Psr\Http\Message\ResponseInterface
+    {
+        $this->view->assign('newMember', new \Gmbit\Staff\Domain\Model\Member());
+        return $this->htmlResponse();
+    }
+
+    public function createAction(\Gmbit\Staff\Domain\Model\Member $newMember): \Psr\Http\Message\ResponseInterface
+    {
+        $this->memberRepository->add($newMember);
+        $this->addFlashMessage('Član je uspešno dodat.');
+        return $this->redirect('list');
+    }
+
 }
